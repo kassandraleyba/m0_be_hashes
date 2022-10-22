67c78fef6c6d829e8d5f2e2c34fb63bf21bca725
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-22 13:32:09 -0600

    Add hashes_2 answers

--- hashes_2.rb
+++ hashes_2.rb
@@ -1,5 +1,14 @@
 # 1- Using the following Hash
 
+# states = {
+#   "Oregon" => "OR",
+#   "Alabama" => "AL",
+#   "New Jersey" => "NJ",
+#   "Colorado" => "CO"
+# }
+
+# Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
+
 states = {
   "Oregon" => "OR",
   "Alabama" => "AL",
@@ -7,14 +16,22 @@ states = {
   "Colorado" => "CO"
 }
 
-# Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-#
-#
-#
+states.each do |states|
+puts "I am from #{states}."
+end
 
 
-# ======================================
-# 2- Using the following Hash
+# # ======================================
+# # 2- Using the following Hash
+
+# birthdays = {
+#   "Cindy" => "June 10",
+#   "Rigo" => "March 31",
+#   "Amos" => "July 21",
+#   "Jeff" => "February 12"
+# }
+
+# # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
 
 birthdays = {
   "Cindy" => "June 10",
@@ -23,14 +40,24 @@ birthdays = {
   "Jeff" => "February 12"
 }
 
-# Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-#
-#
-#
+birthdays.each do |birthdays|
+  puts "Students birthdays: #{birthdays}."
+end
 
 
-# ======================================
-# 3- Using the following Hash
+
+
+# # ======================================
+# # 3- Using the following Hash
+
+# login_statuses = {
+#   "Cindy" => true,
+#   "Rigo" => false,
+#   "Amos" => true,
+#   "Jeff" => true
+# }
+
+# # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
 
 login_statuses = {
   "Cindy" => true,
@@ -39,7 +66,6 @@ login_statuses = {
   "Jeff" => true
 }
 
-# Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
-#
-#
+login_statuses.keys.each do |login_statuses|
+  puts login_statuses
+end
